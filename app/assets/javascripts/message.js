969c16d2399e1fc4c2b26e4e67e2f065995820e1
kosuke-nakata <58108411+kosuke-nakata@users.noreply.github.com>
2019-12-29 18:16:20 +0900

    make_to_pressable_send_button_in_continuous

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -55,6 +55,8 @@ $(function(){
       var html = buildHTML(data);
       $('.messages').append(html);      
       $('form')[0].reset();
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('.new_message__inputbox__send-btn').prop('disabled', false);
     })
 })
 });
