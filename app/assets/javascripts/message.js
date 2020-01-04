e79e2121059d13eefd4e4b4574fba6cab24001ed
kosuke-nakata <58108411+kosuke-nakata@users.noreply.github.com>
2020-01-04 14:25:07 +0900

    acquired_latest_message_browser_message_list_addition

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,80 @@
 $(function(){
-     function buildHTML(message){
-      if ( message.image ) {
-        var html =
-        `<div class="messages__message" data-message-id=${message.id}>
-            <div class="messages__message__info">
-              <div class="messages__message__info__user-name"">
-                ${message.user_name}
-              </div>
-              <div class="messages__message__info__date">
-                ${message.created_at}
-              </div>
-            </div>
-            <div class="messages__message__text">
-              <p class="messages__message__text__conten">
-                ${message.content}
-              </p>
-            </div>
-            <img src=${message.image} >
-          </div>`
-        return html;
-      } else {
-        var html =
-        `<div class="messages__message" data-message-id=${message.id}>
-            <div class="messages__message__info">
-              <div class="messages__message__info__user-name"">
-                ${message.user_name}
-              </div>
-              <div class="messages__message__info__date">
-                ${message.created_at}
-              </div>
-            </div>
-            <div class="messages__message__text">
-              <p class="messages__message__text__conten">
-                ${message.content}
-              </p>
-            </div>
-          </div>`
-        return html;
+  
+  console.log(last_message_id);
+
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+});
+
+      var buildHTML = function(message) {
+        if (message.content && message.image) {
+
+        var html = `<div class="messages__message" data-message-id=` + message.id + `>` +
+          `<div class="messages__message__info">` +
+             `<div class="messages__message__info__user-name">` +
+              message.user_name +
+              `</div>` +
+              `<div class="messages__message__info__date">` +
+                message.created_at +
+              `</div>` +
+            `</div>` +
+            `<div class="messages__message__text">` +
+              `<p class="messages__message__text__content">` +
+                message.content +
+              `</p>` +
+            `<img src="` + message.image + `" class="messages__message__text__image" >` +
+          `</div>` +
+          `</div>`
+        } else if (message.content) {
+        var html = `<div class="messages__message" data-message-id=` + message.id + `>` +
+            `<div class="messages__message__info">` +
+              `<div class="messages__message__info__user-name">` +
+                message.user_name +
+              `</div>` +
+              `<div class="messages__message__info__date">` +
+                message.created_at +
+              `</div>` +
+            `</div>` +
+            `<div class="messages__message__text">` +
+              `<p class="messages__message__text__content">` +
+                message.content +
+              `</p>` +
+            `</div>` +
+          `</div>`
+        } else if (message.image) {
+          var html = `<div class="messages__message" data-message-id=` + message.id + `>` +
+          `<div class="messages__message__info">` +
+            `<div class="messages__message__info__user-name">` +
+              message.user_name +
+            `</div>` +
+            `<div class="messages__message__info__date">` +
+              message.created_at +
+            `</div>` +
+          `</div>` +
+          `<div class="messages__message__text">` +
+            `<img src="` + message.image + `" class="messages__message__text__image" >` +
+          `</div>` +
+        `</div>`
       };
-    }
+      return html;
+    };
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -62,4 +98,3 @@ $(function(){
       alert("メッセージ送信に失敗しました");
   });
 })
-});
